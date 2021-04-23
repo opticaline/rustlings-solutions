2d110a4155b1a01c2a4c1bed7f76d2e165718bd6
zhangxu <opticaline.z@gmail.com>
2021-04-23 17:17:53 +0800

    complete

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -23,6 +22,19 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        let parts = s.splitn(2, ',').collect::<Vec<&str>>();
+        match &parts[..] {
+            [name, age] if !name.is_empty() => {
+                match age.parse().map(|age| Person {
+                    name: name.to_string(),
+                    age,
+                }) {
+                    Ok(p) => Ok(p),
+                    _ => Err("".to_owned()),
+                }
+            }
+            _ => Err("".to_owned()),
+        }
     }
 }
 
